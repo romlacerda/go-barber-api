92635e94465bb3759f5a6031fc2460e1f1c06e62
Romulo Lacerda <rlcrweb@gmail.com>
2020-07-15 23:29:44 -0300

    feat: exception handlers

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,8 +1,9 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
-import { sign, verify } from 'jsonwebtoken';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 interface Request {
   email: string;
@@ -23,13 +24,13 @@ class AuthenticateUserService {
     });
 
     if (!user) {
-      throw new Error('User not found');
+      throw new AppError('User not found', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('User not found');
+      throw new AppError('User not found', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
