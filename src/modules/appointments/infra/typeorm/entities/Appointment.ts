f91604b508a42be238667e1ceb36088bfae72b2e
Romulo Lacerda <rlcrweb@gmail.com>
2020-09-30 09:57:52 -0300

    ddd

--- src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -8,7 +8,7 @@ import {
   JoinColumn,
 } from 'typeorm';
 
-import User from './User';
+import User from '@modules/users/infra/typeorm/entites/User';
 
 @Entity('appointments')
 class Appointment {
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-import routes from './routes';
+import cors from 'cors';
+import routes from '@shared/routes';
 import 'reflect-metadata';
-import './database';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import '@shared/infra/typeorm';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
